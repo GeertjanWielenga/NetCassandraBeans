fb6fba14c1ca18f343f7abc68ac71c44c6549e4c
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2015-07-26 10:42:57 +0200

    removing custom editors and getting started displaying real content

--- src/main/java/com/ncb/RowChildFactory.java
+++ src/main/java/com/ncb/RowChildFactory.java
@@ -1,6 +1,7 @@
 package com.ncb;
 
 import com.datastax.driver.core.ColumnDefinitions;
+import com.datastax.driver.core.DataType;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import java.beans.IntrospectionException;
@@ -13,16 +14,16 @@ import org.openide.nodes.PropertySupport;
 import org.openide.nodes.Sheet;
 import org.openide.nodes.Sheet.Set;
 import org.openide.util.Exceptions;
-import org.openide.util.actions.SystemAction;
 
 class RowChildFactory extends ChildFactory<ColumnDefinitions.Definition> {
 
     private final ColumnDefinitions cds;
-    private List<Row> rows;
+    private ResultSet rs;
+    private String content;
 
     public RowChildFactory(ResultSet rs) {
         this.cds = rs.getColumnDefinitions();
-        this.rows = rs.all();
+        this.rs = rs;
     }
 
     @Override
@@ -45,32 +46,67 @@ class RowChildFactory extends ChildFactory<ColumnDefinitions.Definition> {
     }
 
     private class RowNode extends BeanNode {
+
         ColumnDefinitions.Definition key = null;
+
         public RowNode(ColumnDefinitions.Definition key) throws IntrospectionException {
             super(key);
             this.key = key;
             setDisplayName(key.getName());
         }
+
         @Override
         public PropertySet[] getPropertySets() {
             Set set = Sheet.createPropertiesSet();
             for (int i = 0; i < cds.size(); i++) {
-//                Row r = rows.get(i);
                 ColumnDefinitions.Definition def = cds.asList().get(i);
-                set.put(new PropertySupport.ReadOnly<String>(
-                        def.getName(), String.class, def.getName(), null) {
-                            @Override
-                            public String getValue() throws IllegalAccessException, InvocationTargetException {
-                                String name = def.getName();
-                                return def.getType().toString();
-                            }
-                        });
+                PropertySupport.ReadOnly<String> ValuePropertySupport = ValuePropertySupport(def);
+                ValuePropertySupport.setValue("suppressCustomEditor", Boolean.TRUE);
+                set.put(ValuePropertySupport);
             }
             PropertySet[] original = super.getPropertySets();
             PropertySet[] withLayer = new PropertySet[original.length + 1];
             System.arraycopy(original, 0, withLayer, 0, original.length);
             withLayer[withLayer.length - 1] = set;
             return withLayer;
+        }
+
+        private PropertySupport.ReadOnly<String> ValuePropertySupport(ColumnDefinitions.Definition def) {
+            return new PropertySupport.ReadOnly<String>(
+                    def.getName(), String.class, def.getName(), null) {
+                        @Override
+                        public String getValue() throws IllegalAccessException, InvocationTargetException {
+                            getValueFromRows(rs, def);
+                            return content;
+                        }
+                    };
+        }
+    }
+
+    private void getValueFromRows(ResultSet rs, ColumnDefinitions.Definition cd) {
+        for (Row r : rs.all()) {
+            String name = cd.getName();
+            if (cd.getType() == DataType.cint()) {
+                content = String.valueOf(r.getInt(name));
+            }
+            if (cd.getType() == DataType.varchar()) {
+                content = r.getString(name);
+            }
+            if (cd.getType() == DataType.uuid()) {
+                content = r.getUUID(name).toString();
+            }
+            if (cd.getType() == DataType.cfloat()) {
+                content = String.valueOf(r.getFloat(name));
+            }
+            if (cd.getType() == DataType.cdouble()) {
+                content = String.valueOf(r.getDouble(name));
+            }
+            if (cd.getType() == DataType.list(DataType.text())) {
+                content = "list of items";
+            }
+            if (cd.getType() == DataType.timestamp()) {
+                content = r.getDate(name).toString();
+            }
         }
     }
 
