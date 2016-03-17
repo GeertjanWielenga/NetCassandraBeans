a10c54a7e5662ea83438c5c26cd7f81eabf5653a
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-03-17 23:57:14 +0100

    Icon switching for start-stop of server.

--- src/main/java/com/ncb/CassandraRootNode.java
+++ src/main/java/com/ncb/CassandraRootNode.java
@@ -3,8 +3,10 @@ package com.ncb;
 import com.ncb.server.CentralLookup;
 import com.ncb.server.Startable;
 import com.ncb.server.Stoppable;
+import java.awt.Image;
 import java.io.IOException;
 import javax.swing.Action;
+import org.netbeans.api.annotations.common.StaticResource;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
 import org.openide.actions.NewAction;
@@ -12,9 +14,14 @@ import org.openide.actions.OpenLocalExplorerAction;
 import org.openide.awt.StatusDisplayer;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
+import org.openide.util.ImageUtilities;
+import org.openide.util.Lookup;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
 import org.openide.util.NbBundle;
 import org.openide.util.NbPreferences;
 import org.openide.util.Utilities;
+import org.openide.util.WeakListeners;
 import org.openide.util.actions.SystemAction;
 import org.openide.util.datatransfer.NewType;
 
@@ -23,18 +30,39 @@ import org.openide.util.datatransfer.NewType;
 //        name = "Cassandra", 
 //        iconResource = "com/ncb/logo.png", 
 //        shortDescription = "Cassandra")
-public class CassandraRootNode extends AbstractNode {
+public class CassandraRootNode extends AbstractNode implements LookupListener {
     
     private CassandraServerStartable csStart = new CassandraServerStartable();
     private CassandraServerStoppable csStop = new CassandraServerStoppable();
     
+    @StaticResource
+    private static final String ICONRUNNING = "com/ncb/server-running.png";
+    @StaticResource
+    private static final String ICONSTOPPED = "com/ncb/server-stopped.png";
+    @StaticResource
+    private static final String ICONORIGINAL = "com/ncb/logo.png";
+    
+    private Lookup.Result<Startable> startableResult;
+    private Lookup.Result<Stoppable> stoppableResult;
+
     
     public CassandraRootNode() {
         super(Children.create(new CassandraClusterFactory(), true));
         CentralLookup.getDefault().add(csStart);
         setDisplayName("Cassandra");
         setShortDescription("Cassandra");
-        setIconBaseWithExtension("com/ncb/logo.png");
+        setIconBaseWithExtension(ICONORIGINAL);
+        startableResult = CentralLookup.getDefault().lookupResult(Startable.class);
+        startableResult.addLookupListener(WeakListeners.create(LookupListener.class, this, startableResult));
+        resultChanged(new LookupEvent(startableResult));
+        stoppableResult = CentralLookup.getDefault().lookupResult(Stoppable.class);
+        stoppableResult.addLookupListener(WeakListeners.create(LookupListener.class, this, stoppableResult));
+        resultChanged(new LookupEvent(stoppableResult));
+    }
+    
+    @Override
+    public void resultChanged(LookupEvent le) {
+        fireIconChange();
     }
     
     private class CassandraServerStartable implements Startable {
@@ -43,6 +71,25 @@ public class CassandraRootNode extends AbstractNode {
             CentralLookup.getDefault().remove(csStart);
             StatusDisplayer.getDefault().setStatusText("Starting...");
             CentralLookup.getDefault().add(csStop);
+        }
+    }
+    
+    @Override
+    public Image getIcon(int type) {
+        return switchIcon();
+    }
+    
+    @Override
+    public Image getOpenedIcon(int type) {
+        return switchIcon();
+    }
+
+    private Image switchIcon() {
+        Image original = ImageUtilities.loadImage(ICONORIGINAL);
+        if (!startableResult.allInstances().isEmpty()) {
+            return ImageUtilities.mergeImages(original, ImageUtilities.loadImage(ICONSTOPPED), 7, 7);
+        } else {
+            return ImageUtilities.mergeImages(original, ImageUtilities.loadImage(ICONRUNNING), 7, 7);
         }
     }
     
@@ -58,8 +105,8 @@ public class CassandraRootNode extends AbstractNode {
     @Override
     public Action[] getActions(boolean context) {
         Action[] result = new Action[]{
-//            Utilities.actionsForPath("Actions/CassandraServer").get(0),
-//            Utilities.actionsForPath("Actions/CassandraServer").get(1),
+            Utilities.actionsForPath("Actions/CassandraServer").get(0),
+            Utilities.actionsForPath("Actions/CassandraServer").get(1),
             SystemAction.get(NewAction.class),
             null,
             SystemAction.get(OpenLocalExplorerAction.class)};
